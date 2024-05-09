48f8f99ade644f670335c33beabc889c95f7bfbc
Dev Gupta <devgupta210513@acropolis.in>
2024-05-10 02:24:14 +0530

    fomate in error.jsx at line 6

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <div className='w-[100vw] h-[100vh] bg-richblack-800 flex justify-center items-center '>
-        <p className=' text-richblue-5 font-semibold text-4xl'>Error-404</p>
+        <p className='text-4xl font-semibold  text-richblue-5'>Error-404</p>
     </div>
   )
 }
